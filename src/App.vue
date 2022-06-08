66363ef4e0cbd75a0e5b8357d08c4b16463a9c8b
Sonia <1737438545@qq.com>
2022-06-08 23:20:30 +0800

    初步创建项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>hi </div>
+ <Sonia/>
+</template>
+
+<script>
+import Sonia from './components/Sonia.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Sonia
+  }
+}
+</script>
