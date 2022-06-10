623c9e1e5968c73077640829df60711e5a5297e4
Sonia <1737438545@qq.com>
2022-06-10 21:29:25 +0800

    实现 aside 切换

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,8 +2,7 @@
     <div>
         <Topnav />
         <div class="content">
-            <aside>边栏</aside>
-            <aside>
+            <aside v-if="menuVisible">
                 <h2>组件列表</h2>
                 <ol>
                     <li>
@@ -25,9 +24,14 @@
     </div>
 </template>
 <script lang="ts">
-import Topnav from "../components/Topnav.vue"
+import Topnav from "../components/Topnav.vue";
+import { inject, Ref } from 'vue';
     export default {
-        components: { Topnav }
+    components: { Topnav },
+    setup() {
+        const menuVisible = inject<Ref<boolean>>('menuVisible') // get
+        return { menuVisible }
+    }
     }
 
 </script>
