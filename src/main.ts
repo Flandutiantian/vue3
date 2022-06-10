1c9ddbb6d9184cda06ace8cdcdaf2aa167faff84
Sonia <1737438545@qq.com>
2022-06-10 21:00:59 +0800

    初始化aside的HTML

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import { createWebHashHistory, createRouter } from "vue-router";
-import Sonia from "./components/Sonia.vue";
-import Sonia2 from './components/Sonia2.vue'
+
 
 const history = createWebHashHistory();
 const router = createRouter({
     history: history,
     routes: [
-        { path: "/", component: Sonia },
-        { path: "/xxx", component: Sonia2 },
+
     ],
 });
 const app = createApp(App)
