aed8206ee909a6aacb592e5dbaba05deaf3df463
Sonia <1737438545@qq.com>
2022-06-29 20:34:30 +0800

    解决冲突

--- src/router.ts
+++ src/router.ts
@@ -5,16 +5,13 @@ import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
-<<<<<<< HEAD
-=======
-import DocDemo from "./components/DocDemo.vue";
-import { h } from 'vue';
-import Markdown from './components/Markdown.vue';
->>>>>>> 6f3b1a8f79bf83485677576aa42e989d3ef0cd50
 
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+
+
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 const history = createWebHashHistory();
 const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
@@ -25,14 +22,10 @@ export const router = createRouter({
             path: "/doc",
             component: Doc,
             children: [
-<<<<<<< HEAD
                 { path: "", redirect: '/doc/intro' },
                 { path: "intro", component: md('intro') },
                 { path: "get-started", component: md('get-started') },
                 { path: "install", component: md('install') },
-=======
-                { path: "", component: DocDemo },
->>>>>>> 6f3b1a8f79bf83485677576aa42e989d3ef0cd50
                 { path: "switch", component: SwitchDemo },
                 { path: "button", component: ButtonDemo },
                 { path: "dialog", component: DialogDemo },
